918c52b6d9d8a99645414d61292ead985a964746
akaProgramer <51360466+akaProgramer@users.noreply.github.com>
2020-07-27 21:07:28 +0530

    classes and objects in tkinker

---
+++ tkinter_status_bar.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+def uplaod():
+    statusvar.set("Busy...")
+    sbar.update()
+    import time
+    time.sleep(2)
+    statusvar.set("Ready Now")
+root= Tk()
+root.geometry("600x300")
+root.title("Status Bar")
+
+statusvar= StringVar()
+statusvar.set("Ready")
+sbar= Label(root, textvariable=statusvar, relief=SUNKEN)
+sbar.pack(side=BOTTOM, fill=X )
+Button(root, text="Upload", command=uplaod).pack()
+root.mainloop()
